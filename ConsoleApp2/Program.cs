5ecbbd8b87222238e3d4aa764a52bf29bced875a
s27920 <janekkubisa8@gmail.com>
2024-03-17 12:41:09 +0100

    changed name

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,11 +6,11 @@ class Program
         int i = 0;
         Console.WriteLine("Hello, World!");
         Console.WriteLine(i);
-        Console.WriteLine(max(new []{3, 20, 7, 11, 15}));
+        Console.WriteLine(GetMax(new []{3, 20, 7, 11, 15}));
     }
     
 
-    static int max(int[] arr)
+    static int GetMax(int[] arr)
     {
         int max = Int32.MinValue;
         foreach (int num in arr)
