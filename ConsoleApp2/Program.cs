96fd4d6530ea42c167d2a93601370be1b89da9c1
Jan Kubisa <janekkubisa8@gmail.com>
2024-03-17 12:21:48 +0100

    added avg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,5 +6,19 @@ class Program
         int i = 0;
         Console.WriteLine("Hello, World!");
         Console.WriteLine(i);
+        Console.WriteLine(avg(new []{3, 7, 11, 15, 20}));
+    }
+
+    static double avg(int[] arr)
+    {
+        double sum = 0;
+        int counter = 0;
+        foreach (int i in arr)
+        {
+            sum += i;
+            counter++;
+        }
+
+        return sum / counter;
     }
 }
